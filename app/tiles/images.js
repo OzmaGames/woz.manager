ad5d662f6a34e798da29d9857efb3cf31671eac0
SaraSaif <sarah@c-82-209-154-28.cust.bredband2.com>
2013-10-12 21:05:58 +0200

    Accordion

--- app/tiles/images.js
+++ app/tiles/images.js
@@ -11,5 +11,11 @@ define (function () {
         {src :"http://i559.photobucket.com/albums/ss37/EllenZoccola/Color%20Splash/summertime.jpg"},
         {src :"http://i559.photobucket.com/albums/ss37/EllenZoccola/Color%20Splash/summertime.jpg"},
         {src :"http://i559.photobucket.com/albums/ss37/EllenZoccola/Color%20Splash/summertime.jpg"}
+        {'id' :'1',
+         'words':['animal', 'amb','lady','sun'],
+         'src' :'http://i559.photobucket.com/albums/ss37/EllenZoccola/Color%20Splash/summertime.jpg'},
+        {'id' :'2',
+         'words':['color', 'red','green','summer'],
+         'src' :'http://i559.photobucket.com/albums/ss37/EllenZoccola/Color%20Splash/summertime.jpg'}
     ]
 })
