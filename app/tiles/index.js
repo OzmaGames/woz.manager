fa40d2738dd0dfa4f697078571ab5aaf5096ec36
arafati <ali.prg@gmail.com>
2013-12-22 19:16:56 +0100

    binding fixed

--- app/tiles/index.js
+++ app/tiles/index.js
@@ -1,62 +1,59 @@
 define(['api/datacontext', 'knockout', 'jquery', 'grid', 'plugins/router'], function (ctx, ko, $, router) {
-    
-    var ctor = function () {
+
+   window.ko = ko;
+   var ctor = function () {
       var self = this;
 
       self.setList = ko.observableArray();
       self.selectedSet = ko.observable();
       self.tileList = ko.observableArray();
       self.selectedTile = ko.observable();
-      
-      self.chooseTile = function(tile){
-        console.log("b");
-        self.selectedTile(tile);
+
+      self.chooseTile = function (tile) {
+         self.selectedTile(tile);
       }
-      
-      
-      console.log("out");
+
       self.rows = ko.computed(function () {
-        var rows = [],
-        currentRow,
-        colLength = 6;
-        
-        if (self.selectedTile()) {
+         var rows = [],
+         currentRow,
+         colLength = 6;
+
+         if (self.selectedTile()) {
             for (var i = 0, j = self.selectedTile().related.length; i < j; i++) {
-                if (i % colLength === 0) {
-                   if (currentRow) {
+               if (i % colLength === 0) {
+                  if (currentRow) {
                      rows.push(currentRow);
-                    }
-                    currentRow = [];
+                  }
+                  currentRow = [];
+               }
+               currentRow.push(self.selectedTile().related[i]);
             }
-            currentRow.push(self.selectedTile().related[i]);
-           }
-          if (currentRow) {
-              rows.push(currentRow);
-           }
-           console.log("in");
-          return rows;
-        }
-    });
-      
-      self.home = function() {
+            if (currentRow) {
+               rows.push(currentRow);
+            }
+            return rows;
+         }
+      });
+
+      self.home = function () {
          console.log("home");
-        router.navigate("#words");
+         router.navigate("#words");
       }
-     
-     self.remove = function (e, word) {
-        console.log("a");
-        e.preventDefault();
-        self.rows.remove(word);
-        
-    }
-      
-    }
-      
-    
-    ctor.prototype.compositionComplete = function () {
+
+      self.remove = function (word) {
+         var relatedWords = self.selectedTile().related;
+         var pos = relatedWords.indexOf(word);
+         relatedWords.splice(pos, 1);
+
+         self.selectedTile.valueHasMutated();
+      }
+   }
+
+
+   ctor.prototype.compositionComplete = function () {
       grid().init();
    }
-   
+
    ctor.prototype.activate = function () {
       var base = this;
 
