4a73bdfee8156af0ab85a7a3446f350a1daebce2
SaraSaif <sarah@sara-seifis-MacBook-Pro.local>
2014-01-07 14:54:59 +0100

    up

--- app/shell.js
+++ app/shell.js
@@ -1,5 +1,6 @@
-﻿define(['plugins/router'], function (router) {
+﻿define(['plugins/router', 'add/form','durandal/app'], function (router, form, app) {
     return {
+        addCatCal: function(){form.show().then(function(){console.log('a')})},
         router: router,
         activate: function () {
             return router.map([
@@ -8,8 +9,7 @@
                 { route: 'rules',                       moduleId: 'rules/index',            title: 'Rules',             nav: true },
                 { route: 'tile-gallery',                moduleId: 'tiles/index',            title: 'Tiles',             nav: true },
                 { route: 'create-rule',                 moduleId: 'rules/form'},
-                { route: 'edit-rule/:id',               moduleId: 'rules/form'},
-                { route: 'add-Category/Collection',     moduleId: 'add/index',             title: 'Add Category/Collection',  nav:true}
+                { route: 'edit-rule/:id',               moduleId: 'rules/form'}
             
                 
             ]).buildNavigationModel()
