d96a07fb483aa23de859dd9796e3433c8c9c8579
arafati <ali.prg@gmail.com>
2014-05-11 21:18:11 +0200

    BBD Fixed

--- app/api/bootstrapDropDown.js
+++ app/api/bootstrapDropDown.js
@@ -1,31 +1,20 @@
-define(['knockout'], function (ko) {
-    ko.bindingHandlers["bootstrapDropDown"] = {
-        init: function (element, valueAccessor, allBindings, data, context) { 
-                                          
-            ko.computed({
-                disposeWhenNodeIsRemoved: element,
-                read: function () {
-                   var bindings = allBindings();
-                   //if options presents in data-binding then listen to its changes (computed will listen to its changes as soon as you read from an obs)
-                   if ( bindings.options ) {
-                      // data-bind="options:foo, value:selectedClass, bootstrapDropDown: selectpickerOptions"
-                      //unwrap will unwrap the object if it is an obs, or return its value if it is not
-                      var options = ko.unwrap( bindings.options );
-                      setTimeout( function () {
-                         var selectpickerOptions = valueAccessor();
-                         $( element ).selectpicker( selectpickerOptions );
-                      }, 200 );
-                   } else {
-                      // data-bind="bootstrapDropDown: {options: observableArray, selectpickerOptions: selectpickerOptions}"
-                      // alternatively attach to options
-                      setTimeout( function () {
-                         var data = valueAccessor();
-                         var options = ko.unwrap( data.options );
-                         $(element).selectpicker();
-                      }, 200 );                      
-                   }
-                }
-            });
-        }
-    };    
-});
+define( ['knockout'], function ( ko ) {
+   ko.bindingHandlers["bootstrapDropDown"] = {
+      init: function ( element, valueAccessor, allBindings, data, context ) {
+
+         ko.computed( {
+            disposeWhenNodeIsRemoved: element,
+            read: function () {
+               // data-bind="bootstrapDropDown: {options: observableArray, selectpickerOptions: selectpickerOptions}"               
+               var data = valueAccessor();
+               var options = ko.utils.unwrapObservable( data.options );
+
+               setTimeout( function () {
+                  //wait for ko to bind other stuff
+                  $( element ).selectpicker( data.selectpickerOptions || undefined );
+               }, 1 );
+            }
+         } );
+      }
+   };
+} );
