4ea2d67e29f7257dd5fc98eab60d4a68ca5b4522
arafati <ali.prg@gmail.com>
2014-05-13 14:50:22 +0200

    BDD

--- app/api/bootstrapDropDown.js
+++ app/api/bootstrapDropDown.js
@@ -1,20 +1,39 @@
 define( ['knockout'], function ( ko ) {
-   ko.bindingHandlers["bootstrapDropDown"] = {
+   ko.bindingHandlers["BDD"] = {
       init: function ( element, valueAccessor, allBindings, data, context ) {
-return;
+         var $el = $( element );
+
+         if ( $el.data( "BDD" ) ) return;
+         $el.data( "BDD", true );
+
          ko.computed( {
             disposeWhenNodeIsRemoved: element,
             read: function () {
-               // data-bind="bootstrapDropDown: {options: observableArray, selectpickerOptions: selectpickerOptions}"               
-               var data = valueAccessor();
-               var options = ko.utils.unwrapObservable( data.options );
+               // data-bind="BDD: {options: observableArray, spOptions: selectpickerOptions}"               
+               var allBindingData = allBindings();               
+               var data = allBindingData.BDD || allBindingData.options;
+               var options = ko.utils.unwrapObservable( data.options || data );
 
                setTimeout( function () {
                   //wait for ko to bind other stuff
-                  $( element ).selectpicker( data.selectpickerOptions || undefined );
-               }, 1 );
+                  if ( $( element ).data( "selectpicker" ) ) {
+                     $( element ).selectpicker( 'refresh' );
+                  } else {
+                     $( element ).selectpicker( data.spOptions || undefined );
+                  }
+               }, 100 );
             }
          } );
       }
    };
+
+   var optionsInit = ko.bindingHandlers["options"].init;
+
+   ko.bindingHandlers["options"].init = function () {
+      var result = optionsInit.apply( this, arguments );
+
+      ko.bindingHandlers.BDD.init.apply( this, arguments );
+
+      return result;
+   }
 } );
